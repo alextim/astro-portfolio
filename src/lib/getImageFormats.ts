1e358a36298c6c4a818a7849d8f8ff74cfb471aa
Alex Tim <buildmnt@gmail.com>
2022-03-14 00:12:05 +0200

    gh

--- src/lib/getImageFormats.ts
+++ src/lib/getImageFormats.ts
@@ -1,5 +1,5 @@
-const getImageFormats = (): string[] => {
-  const s = process.env.IMAGE_FORMATS || 'webp,jpg';
+const getImageFormats = (formats: string | undefined): string[] => {
+  const s = formats || process.env.IMAGE_FORMATS || 'webp,jpg';
   return s.split(',').map((s) => s.trim());
 }
 
