1b8df5b7fda2d35f0d1ce4e3ab7fc6f581960a8b
Alex Tim <buildmnt@gmail.com>
2022-03-21 18:42:11 +0200

    'auto'

--- src/lib/getImageFormats.ts
+++ src/lib/getImageFormats.ts
@@ -1,11 +1,4 @@
-// import { loadEnv } from 'vite';
-
-// formats: string | undefined
 const getImageFormats = (): string[] => {
-  // const mode = import.meta.env.MODE;
-  // const env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-  // const env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-  // PUBLIC_
   const imageFormats = process.env.PUBLIC_IMAGE_FORMATS || import.meta.env.PUBLIC_IMAGE_FORMATS;
   return imageFormats.split(',').map((s) => s.trim());
 };
