e552dfaa5dc07fe1ab8b9b97c5615ee5f81f3c39
Alex Tim <buildmnt@gmail.com>
2022-03-29 20:05:48 +0300

    'auto'

--- src/helpers/getImageFormats.ts
+++ src/helpers/getImageFormats.ts
@@ -1,6 +1,8 @@
 const getImageFormats = (): string[] => {
   const imageFormats = process.env.PUBLIC_IMAGE_FORMATS || import.meta.env.PUBLIC_IMAGE_FORMATS;
-  return imageFormats.split(',').map((s) => s.trim());
+  const result = imageFormats.split(',').map((s) => s.trim());
+  console.log('getImageFormats', result);
+  return result;
 };
 
 export default getImageFormats;
