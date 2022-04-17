3c3fe2423327abaedfd5e732b30d6b2cf98f3454
Alex Tim <buildmnt@gmail.com>
2022-04-18 00:10:22 +0300

    'auto'

--- src/helpers/getImageFormats.ts
+++ src/helpers/getImageFormats.ts
@@ -1,6 +1,10 @@
-const getImageFormats = (): string[] => {
-  console.log('pocess:',process.env.PUBLIC_IMAGE_FORMATS);
-  console.log('imp:', import.meta.env.PUBLIC_IMAGE_FORMATS);
+/**
+ * Returns array of image formats from .env
+ * It should be used only in .astro files
+ *
+ * @returns {Array<string>}
+ */
+const getImageFormats = () => {
   const imageFormats = import.meta.env.PUBLIC_IMAGE_FORMATS;
   const result = imageFormats.split(',').map((s) => s.trim());
   return result;
