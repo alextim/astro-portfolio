cf97a1fae5c7bfb06a3ab29a668a7510886145e9
Alex Tim <buildmnt@gmail.com>
2022-04-19 16:54:32 +0300

    yaml

--- src/api/yamls.ts
+++ src/api/yamls.ts
@@ -1,11 +1,17 @@
+import fg from 'fast-glob';
 import getYaml from './utils/getYaml';
 
+let contacts: Promise<Contacts>;
 export async function fetchContacts(): Promise<Contacts> {
-  const result = await getYaml('content/data/contacts.yaml');
-  return result as Promise<Contacts>;
+  if (contacts) {
+    
+  }
+  // return contacts || (contacts = await getYaml('content/data/contacts.yaml'));
 }
 
 export async function fetchSocialLinks(locale: string): Promise<SocialLinks> {
+  const entries = await fg(['content/data/locales/social-links/social-links.??.yaml'], { dot: true });
+  console.log(entries);
   const result = await getYaml(`content/data/locales/social-links/social-links.${locale}.yaml`);
   return result as Promise<SocialLinks>;
 }
